4406e8146e3e8b9af06a7e9a3331252b58de7b94
farismusic <44246682+farismusic@users.noreply.github.com>
2018-12-19 02:55:54 +0100

    Problemi sa bazom ispravljeni

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,12 +1,27 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Gradovi su:\n" + ispisiGradove());
-        glavniGrad();
+        //System.out.println("Gradovi su:\n" + ispisiGradove());
+        //glavniGrad();
+        String url = "jdbc:sqlite:src/ba.unsa.etf.rpr/baza.db";
+        String upit = "SELECT naziv FROM drzava d, grad g WHERE d.glavni_grad = g.id";
+        try {
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery(upit);
+
+            while(result.next()) {
+                String naziv = result.getString(2);
+                System.out.println (naziv);
+            }
+        }catch (SQLException e){
+            System.out.println("nema");
+        }
     }
 }
